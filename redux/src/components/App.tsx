7935dbc1f5c61c793781a3603cb69c7c3c683531
cswithen <cody.swithenbank@gmail.com>
2023-03-25 12:14:59 -0700

    feat(redux): project complete

--- redux/src/components/App.tsx
+++ redux/src/components/App.tsx
@@ -1,13 +1,13 @@
 import { Provider } from "react-redux";
 import { store } from "../state";
-import RepositoresList from "./RepositoriesList";
+import RepositoriesList from "./RepositoriesList";
 
 function App() {
   return (
     <Provider store={store}>
       <div>
         <h1>Search For a Package</h1>
-        <RepositoresList />
+        <RepositoriesList />
       </div>
     </Provider>
   );
--- redux/src/components/RepositoriesList.tsx
+++ redux/src/components/RepositoriesList.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
-  const state = useSelector((state: any) => state.repositories);
-
-  console.log(state);
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -21,6 +21,15 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && (
+        <ul>
+          {data.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
